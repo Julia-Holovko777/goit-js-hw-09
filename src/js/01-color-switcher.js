33cb6bf2e4609397d82443858874e29db749b0e2
Julia-Holovko777 <golovko.j27@gmail.com>
2023-11-26 15:06:59 +0200

    add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', startChangingColor);
+btnStop.addEventListener('click', stopChangingColor);
+
+let timer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startChangingColor() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timer = setInterval(changeBackgroundColor, 1000);
+}
+function stopChangingColor() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timer);
+}
+
+function changeBackgroundColor() {
+  document.body.style.background = getRandomHexColor();
+}
