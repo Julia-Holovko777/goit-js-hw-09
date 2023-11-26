e0a1ac7a7fefea6751ae5e9d752c2910c97b03db
Julia-Holovko777 <139575105+Julia-Holovko777@users.noreply.github.com>
2023-11-26 13:07:37 +0000

    Deploying to gh-pages from @ Julia-Holovko777/goit-js-hw-09@33cb6bf2e4609397d82443858874e29db749b0e2 🚀

---
+++ 01-color-switcher.73c29221.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}));var a=null;function n(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.73c29221.js.map
